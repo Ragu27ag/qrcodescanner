405371bd9dbb41fd048b4d7c2479a802bbde1c15
Ragunath <raguyuvi27@gmail.com>
2023-12-21 11:51:34 +0530

    first

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { QrReader } from "react-qr-reader";
+import { useState } from "react";
 
 function App() {
+  const [qrData, setQrData] = useState("");
+
+  const scan = (data) => {
+    if (data) {
+      setQrData(data);
+    }
+  };
+
+  const handleError = (error) => {
+    console.log(error);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      {" "}
+      <QrReader onResult={scan} onError={handleError} scanDelay={3000} />
+      <p>{qrData}</p>
+    </>
   );
 }
 
